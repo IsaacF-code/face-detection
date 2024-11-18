ed72442067b1f816c6d0b40950e29e8f833a63c3
Isaac Freires Santos <isaacfreires@gmail.com>
2024-11-18 15:41:30 -0300

    📚 docs: Adicionado comentários no código

--- face_detection.py
+++ face_detection.py
@@ -1,7 +1,9 @@
 import cv2
 
+# Classificador de rosto pré-treinado
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
+# Serve para usar a câmera
 capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while True:
@@ -10,17 +12,25 @@ while True:
     if not retangule:
         print("Erro ao capturar frame da webcam.")
         break
+    
+    # Converte em escala de cinza
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detectar objetos (rostos)
     faces = face_classifier.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(25, 25))
 
+    # Serve para definir a posição do rosto e adicionar o retângulo verde no rosto
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4) 
 
     cv2.imshow('Webcam: ', frame)
 
+    # Pressione a tecla "q" para fechar o programa
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
+# Libera a câmera do sistema
 capture.release()
+
+# Fecha as janelas abertas pelo opencv
 cv2.destroyAllWindows()
